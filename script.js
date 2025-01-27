6f8fcc6ff88d6fe83e6f1b73d1d5992ff11206af
ユズ・アカツ <161923118+YuzuAkach@users.noreply.github.com>
2025-01-27 11:19:58 +0700

    Speed insights update

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+window.si = window.si || function () { (window.siq = window.siq || []).push(arguments); };
 <script defer src="/_vercel/insights/script.js"></script>
