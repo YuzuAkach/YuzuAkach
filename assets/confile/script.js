5cd5d25c4018f0f32bf402bb152efcc47863d42c
ユズ・クリトリー <161923118+YuzuAkach@users.noreply.github.com>
2025-12-31 00:31:20 +0000

    Webpage migration

--- assets/confile/script.js
+++
@@ -1,3 +0,0 @@
-window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
-window.si = window.si || function () { (window.siq = window.siq || []).push(arguments); };
-<script defer src="/_vercel/insights/script.js"></script>
